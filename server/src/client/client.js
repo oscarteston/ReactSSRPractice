ad07f0a03392dc3ee7644277c4a6a95fc0b27c39
Oscar Testón Rodríguez <oscar.teston@accedo.tv>
2019-08-29 12:15:36 +0200

    add redux to show a userList

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
@@ -5,6 +6,7 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import Routes from './Routes';
+import reducers from './reducers';
 
 const store = createStore(reducers, {}, applyMiddleware(thunk));
 
