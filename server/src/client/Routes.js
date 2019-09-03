d3d40a8a403e0886993dfe94694af0d39a6f4abd
Oscar Testón Rodríguez <oscar.teston@accedo.tv>
2019-09-03 16:59:32 +0200

    Load the redux store in the server side

--- server/src/client/Routes.js
+++ server/src/client/Routes.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './components/Home';
-import UsersList from './components/UsersList';
-export default () => {
-    return (
-        <div>
-            <Route exact path='/' component={Home} />
-            <Route exact path='/users' component={UsersList} />
-        </div>
-    )
-}
+import UsersList, { loadData } from './components/UsersList';
+// export default () => {
+//     return (
+//         <div>
+//             <Route exact path='/' component={Home} />
+//             <Route exact path='/users' component={UsersList} />
+//         </div>
+//     )
+// }
 
 // https://react-ssr.api.herokuapp.com
+
+export default [
+    {
+        path: '/',
+        component: Home,
+        exact: true
+    },
+    {
+        loadData,
+        path: '/users',
+        component: UsersList
+    }
+];
+    
